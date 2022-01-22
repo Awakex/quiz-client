1fbdabb5af469405aea51cf9cbc3feb02203ee41
Igor <awkworkx@gmail.com>
2022-01-22 19:08:22 +0300

    updated url

--- src/core/api/auth-api.ts
+++ src/core/api/auth-api.ts
@@ -2,9 +2,9 @@ import {Request} from "../request";
 
 export const AuthAPI = {
     login: (authDto: AuthDto) => {
-        return Request.post(`/auth/login`, authDto);
+        return Request.post(`auth/login`, authDto);
     },
     registration: (authDto: AuthDto) => {
-        return Request.post(`/auth/registration`, authDto);
+        return Request.post(`auth/registration`, authDto);
     },
 };
